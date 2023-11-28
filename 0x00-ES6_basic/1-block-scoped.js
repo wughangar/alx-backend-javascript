fb57295c07f7962fd56f2afcae70797e2fe89566
wughangar <wughangar.rose@gmail.com>
2023-11-28 13:14:16 +0300

    lint requirements

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,9 +3,9 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    const newtask = true;
-    const newtask2 = false;
-    return [newtask, newtask2];
+    const task = true;
+    const task2 = false;
+    return [task, task2];
   }
   return [task, task2];
 }
