1bca4ccd0959fb0cf178be49eb2c64f769ee1f31
wughangar <wughangar.rose@gmail.com>
2023-12-01 11:19:33 +0300

    handling multiple promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,13 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then(([photoResult, userResult]) => {
+      const { body } = photoResult;
+      const { firstName, lastName } = userResult;
+      console.log(`${body} ${firstName} ${lastName}`);
+    })
+    .catch(() => {
+      console.error('Signup system offline');
+    });
+}
