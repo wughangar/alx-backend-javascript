ba2810d7d97649ccac2e3d25f132ffff3a575809
wughangar <wughangar.rose@gmail.com>
2023-12-01 15:27:49 +0300

    class building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,22 +1,20 @@
 export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
-    this._checkEvacuationWarningMessage();
   }
 
   get sqft() {
     return this._sqft;
   }
 
-  _checkEvacuationWarningMessage() {
-    if (this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
-  }
-
   /* eslint-disable class-methods-use-this */
   evacuationWarningMessage() {
     throw new Error('Class extending Building must override evacuationWarningMessage');
   }
-  /* eslint-enable class-methods-use-this */
+}
+/* eslint-disable class-methods-use-this */
+export class room extends Building {
+  evacuationWarningMessage() {
+    return 'Danger: Evacuate now!';
+  }
 }
