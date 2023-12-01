c78d51064892f2827c8e88e7af568eed8525f503
wughangar <wughangar.rose@gmail.com>
2023-12-01 15:40:48 +0300

    inheritance

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,23 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    if (typeof sqft !== 'number' || typeof floors !== 'number') {
+      throw new Error('Incorrect data types');
+    }
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors.`;
+  }
+}
