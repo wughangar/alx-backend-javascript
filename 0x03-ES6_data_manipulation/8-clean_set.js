1bb86afb54142e3ac337010a16c5e88d867be7ff
wughangar <wughangar.rose@gmail.com>
2023-12-05 08:32:08 +0000

    clean set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,7 @@
+export default function cleanSet(set, startString) {
+  const filteredValues = Array.from(set)
+    .filter((value) => value.startsWith(startString))
+    .map((value) => value.slice(startString.length));
+
+  return filteredValues.join('-');
+}
