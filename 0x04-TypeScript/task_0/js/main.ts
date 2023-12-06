9115920e4a665a9e1a5fb76cc84b6115b80e9cd7
wughangar <wughangar.rose@gmail.com>
2023-12-06 09:00:46 +0000

    creating interface for a student

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,42 @@
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  loaction: string;
+}
+
+const student1: Student = {
+  firstName = 'Loki',
+  lastName = 'Rose',
+  age = 20,
+  location = 'Kenya',
+};
+
+const student2 : Student = {
+  firstName = 'Agather',
+  lastName = 'Chari',
+  age = 25,
+  location = 'Kenya',
+};
+
+const studentList: Student[] = [student1, student2];
+
+onst renderTable = () => {
+  const table = document.createElement('table');
+  const tableBody = document.createElement('tbody');
+
+  studentsList.forEach((student) => {
+    const row = tableBody.insertRow();
+    const cellFirstName = row.insertCell();
+    const cellLocation = row.insertCell();
+
+    cellFirstName.textContent = student.firstName;
+    cellLocation.textContent = student.location;
+    tableBody.appendChild(row);
+  });
+
+  table.appendChild(tableBody);
+  document.body.appendChild(table);
+};
+
+renderTable();
