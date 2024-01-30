86f2cb0871e2814bc1286f716704096e58620ec2
wughangar <wughangar.rose@gmail.com>
2024-01-30 02:39:47 +0000

    final code

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,15 +1,12 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-if (process.stdin.isTTY) {
-  process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString().trim()}`);
-    process.exit();
-  });
-} else {
-  process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString().trim()}`);
-    process.exit();
-  });
+process.stdin.on('data', (data) => {
+  const input = data.toString().trim();
+  process.stdout.write(`Your name is: ${input}\n`);
+  process.exit();
+});
+
+if (!process.stdin.isTTY) {
   process.on('exit', () => {
     process.stdout.write('This important software is now closing\n');
   });
