c6d67e8d5f08df4ec298ef0b5d5fb6acdbbacf50
wughangar <wughangar.rose@gmail.com>
2024-01-30 07:58:40 +0000

    set header

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.setHeader(200, { 'Content-Type': 'text/plain' });
 
   res.end('Hello Holberton School!');
 });
