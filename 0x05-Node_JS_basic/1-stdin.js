9a7b5c192fa1ba5377361482fe107e2ad8781479
wughangar <wughangar.rose@gmail.com>
2024-01-30 01:19:24 +0000

    using process for stdin and stdout

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,16 @@
-// using process stdin
+// 1. Using Process stdin
 
 const readline = require('readline');
 
-const line1 = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
 });
 
-line1.question('Welcome to Holberton School, what is your name?\n ', (answer) =>{
-	console.log(`Your name is: ${answer}`);
-	line1.close();
-	console.log('This important software is now closing');
+console.log('Welcome to Holberton School, what is your name?');
+
+rl.question('', (answer) => {
+  console.log(`Your name is: ${answer}`);
+  rl.close();
+  console.log('This important software is now closing');
 });
