fccd29a2d416a28ccce3f600399e51383f95db14
wughangar <wughangar.rose@gmail.com>
2024-01-30 07:49:23 +0000

    http app

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,8 +1,14 @@
-const app = require('http');
+const http = require('http');
 
-app.createServer(function (req, res) {
-  res.write('Hello Holberton School!');
-  res.end();
-}).listen(1245);
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  res.end('Hello Holberton School!\n');
+});
+
+const port = 1245;
+app.listen(port, () => {
+  console.log(`Server is listening on http://localhost:${port}/`);
+});
 
 module.exports = app;
