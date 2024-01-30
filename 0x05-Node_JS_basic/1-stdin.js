fff7443dce0ec813f8636f52a8416af8d6ed4d66
wughangar <wughangar.rose@gmail.com>
2024-01-30 02:14:04 +0000

    trim

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,8 +1,8 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdout.write('Welcome to Holberton School, what is your name?' + '\n');
 
 if (process.stdin.isTTY) {
   process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.stdout.write(`Your name is: ${data.toString().trim()}`);
     process.exit();
   });
 } else {
