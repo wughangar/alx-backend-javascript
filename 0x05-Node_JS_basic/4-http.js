8f660c6a332f88cf522279e177aa5a8f792112e2
wughangar <wughangar.rose@gmail.com>
2024-01-30 07:56:21 +0000

    small http server using node

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -3,12 +3,13 @@ const http = require('http');
 const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
 
-  res.end('Hello Holberton School!\n');
+  res.end('Hello Holberton School!');
 });
 
 const port = 1245;
-app.listen(port, () => {
-  console.log(`Server is listening on http://localhost:${port}/`);
-});
+const hostname = '127.0.0.1';
+
+app.listen(port, hostname);
+
 
 module.exports = app;
